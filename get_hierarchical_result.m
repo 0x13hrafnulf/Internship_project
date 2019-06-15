775850d4d38f548e5f1287368a4def53b0f14cf9
umarbaevB <32370060+umarbaevB@users.noreply.github.com>
2019-06-15 14:17:45 +0600

    Update source file

--- get_hierarchical_result.m
+++ get_hierarchical_result.m
@@ -1,4 +1,4 @@
-function labels =  get_hierarchial_result(input_matrix, n)
+function labels =  get_hierarchical_result(input_matrix, n)
     
     tic
     labels = clusterdata(input_matrix(:,1:2), 'Linkage', 'centroid', 'MaxClust', n);
